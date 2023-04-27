8be43ddcf2b41c457cb72920dd056fb5e3c60ed7
4K3YM4L <127394223+4K3YM4L@users.noreply.github.com>
2023-04-27 21:24:48 +0800

    Update wiznet.sh

--- wiznet.sh
+++ wiznet.sh
@@ -11,57 +11,116 @@ COLORS=("\033[0;31m" "\033[0;32m" "\033[0;33m" "\033[0;34m" "\033[0;35m" "\033[0
 
 # Define the reset color code
 RESET_COLOR="\033[0m"
+echo -e "\e[1;34m"
+cat << "EOF"
 
-# Prompt for IP address
-read -p "Do you want to use your local IP? (Y/N): " use_local_ip
+██╗    ██╗██╗███████╗███╗   ██╗███████╗████████╗
+██║    ██║██║╚══███╔╝████╗  ██║██╔════╝╚══██╔══╝
+██║ █╗ ██║██║  ███╔╝ ██╔██╗ ██║█████╗     ██║   
+██║███╗██║██║ ███╔╝  ██║╚██╗██║██╔══╝     ██║   
+╚███╔███╔╝██║███████╗██║ ╚████║███████╗   ██║   
+ ╚══╝╚══╝ ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝   ╚═╝   
 
-if [[ $use_local_ip == "Y" || $use_local_ip == "y" ]]; then
-  ip=$(hostname -I | awk '{print $1}')
-  
-else
-  read -p "Enter IP address to lookup: " ip
-fi
+EOF
+echo -e "\033[0m"
+# Define the path to the IP address file
+IP_FILE="ip.txt"
 
+# Function to prompt the user to select an IP address
+function choose_ip_address() {
+  echo "How do you want to choose an IP address?"
+  select choice in "Use local IP" "Manually enter a single IP" "Choose from a file"
+  do
+    case $choice in
+      "Use local IP")
+        ip=$(hostname -I | awk '{print $1}')
+        break
+        ;;
+      "Manually enter a single IP")
+        read -p "Enter the IP address: " ip
+        break
+        ;;
+      "Choose from a file")
+        # Read the IP addresses from the file into an array
+        ips=($(cat "$IP_FILE"))
+
+        # Prompt the user to select an IP address from the list
+        echo "Select an IP address from the list:"
+        select ip in "${ips[@]}"
+        do
+          # Check if the user has made a selection
+          if [[ -n "$ip" ]]
+          then
+            break
+          fi
+        done
+        break
+        ;;
+      *)
+        echo "Invalid choice, please try again."
+        ;;
+    esac
+  done
+}
+
+# Initial IP address selection
+choose_ip_address
 
 # Loop to perform multiple lookups
 while true
 do
 # Wiznet title in ASCII art
-echo -e "\e[1;31m"
+echo -e "\e[1;34m"
 cat << "EOF"
 
-'|| '||'  '|'  ||          '|.   '|'           .   
- '|. '|.  .'  ...  ......   |'|   |    ....  .||.  
-  ||  ||  |    ||  '  .|'   | '|. |  .|...||  ||   
-   ||| |||     ||   .|'     |   |||  ||       ||   
-    |   |     .||. ||....| .|.   '|   '|...'  '|.' 
+██╗    ██╗██╗███████╗███╗   ██╗███████╗████████╗
+██║    ██║██║╚══███╔╝████╗  ██║██╔════╝╚══██╔══╝
+██║ █╗ ██║██║  ███╔╝ ██╔██╗ ██║█████╗     ██║   
+██║███╗██║██║ ███╔╝  ██║╚██╗██║██╔══╝     ██║   
+╚███╔███╔╝██║███████╗██║ ╚████║███████╗   ██║   
+ ╚══╝╚══╝ ╚═╝╚══════╝╚═╝  ╚═══╝╚══════╝   ╚═╝   
 
 EOF
 echo -e "\033[0m"
 
 # Green color for the menu
+RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+CYAN='\033[0;36m'
 RESET_COLOR='\033[0m'
-echo " IP Address: $ip"
-echo -e "${GREEN}+========================================+"
-echo "| 1.  Dig                                |"
-echo "| 2.  Host                               |"
-echo "| 3.  Whois                              |"
-echo "| 4.  NSLookup                           |"
-echo "| 5.  Traceroute                         |"
-echo "| 6.  Nmap                               |"
-echo "| 7.  Netcat                             |"
-echo "| 8.  Tcpdump                            |"
-echo "| 9.  Exit                               |"
-echo "+========================================+"
+# Print header
+echo -e "${CYAN}╔═════════════════════════════════════════╗"
+echo -e "          $ip          "
+echo -e "╚═════════════════════════════════════════╝${RESET_COLOR}"
+
+
+echo -e "${GREEN}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓"
+echo "┃  0.  Change IP                          ┃  "
+echo "┃  1.  Dig                                ┃ "
+echo "┃  2.  Host                               ┃ "
+echo "┃  3.  Whois                              ┃ "
+echo "┃  4.  NSLookup                           ┃ "
+echo "┃  5.  Traceroute                         ┃ "
+echo "┃  6.  Nmap                               ┃ "
+echo "┃  7.  Netcat                             ┃ "
+echo "┃  8.  Tcpdump                            ┃ "
+echo "┃  9.  Exit                               ┃ "
+echo "┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛"
     read -p "Enter a number (1-9): " lookup_type
 
-    # Check if user wants to exit
-    if [[ "$lookup_type" == "9" ]]
-    then
-        echo "Exiting..."
-        break
-    fi
+    # Check if user wants to exit or change ip
+  if [[ "$lookup_type" == "0" ]]
+  then
+    choose_ip_address
+  fi
+
+  # Check if user wants to exit
+  if [[ "$lookup_type" == "9" ]]
+  then
+    echo "Exiting..."
+    break
+  fi
 
     # Select a random color code from the array
     color=${COLORS[$RANDOM % ${#COLORS[@]}]}
